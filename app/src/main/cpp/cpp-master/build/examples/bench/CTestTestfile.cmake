618b5d67ea3f4502f34a283f33e893c647cbed55
yamanobora <muranotayori@gmail.com>
2026-03-15 00:12:48 +0900

    Add files via upload

---
+++ app/src/main/cpp/cpp-master/build/examples/bench/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/whisper.cpp-master/examples/bench
+# Build directory: C:/Users/user/Desktop/whisper.cpp-master/build/examples/bench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
